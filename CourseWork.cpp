d2fce4193e1f1f1d616b6f7ed11017790997c6a6
blirex <vanazubarev883@gmail.com>
2025-03-04 23:33:25 +0200

    Add menu

--- CourseWork.cpp
+++ CourseWork.cpp
@@ -2,5 +2,13 @@
 using namespace std;
 
 int main() {
+    cout << "============================" << endl;
+    cout << "      Cipher Caesar" << endl;
+    cout << "============================" << endl;
+    cout << "1. Encrypt phrase: " << endl;
+    cout << "2. Exit" << endl;
+    cout << "Choose option: ";
 
+    int choice;
+    cin >> choice;
 }
