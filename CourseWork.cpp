c9bc8f89e26d3457689676a8a2d8fc423d2141b5
blirex <vanazubarev883@gmail.com>
2025-06-08 18:57:40 +0300

    Added support for other languages and fix errors with localisation

--- CourseWork.cpp
+++ CourseWork.cpp
@@ -1,121 +1,166 @@
 ﻿#include <iostream>
 #include <string>
+#include <locale>
+#include <io.h>
+#include <fcntl.h>
+
 using namespace std;
 
+const wstring RESET = L"\033[0m";
+const wstring RED = L"\033[31m";
+const wstring GREEN = L"\033[32m";
+const wstring CYAN = L"\033[36m";
+const wstring YELLOW = L"\033[33m";
+const wstring MAGENTA = L"\033[35m";
+const wstring BOLD = L"\033[1m";
 
-const string RESET = "\033[0m";
-const string RED = "\033[31m";
-const string GREEN = "\033[32m";
-const string CYAN = "\033[36m";
-const string YELLOW = "\033[33m";
-const string MAGENTA = "\033[35m";
-const string BOLD = "\033[1m";
 
-string caesarCipher(const string& text, int startKey, int multiplier, int divisor) {
-    string encryptedText = "";
-    int key = startKey;
+const wstring LATIN_UPPER = L"ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const wstring LATIN_LOWER = L"abcdefghijklmnopqrstuvwxyz";
 
-    for (char charValue : text) {
-        if (isalpha(charValue)) {
-            int shift = (key * multiplier) / divisor;
-            if (islower(charValue)) {
-                charValue = ((charValue - 'a' + shift) % 26) + 'a';
-            }
-            else {
-                charValue = ((charValue - 'A' + shift) % 26) + 'A';
-            }
-        }
-        encryptedText += charValue;
-        key++;
+const wstring RUSSIAN_UPPER = L"АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+const wstring RUSSIAN_LOWER = L"абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+
+const wstring UKRAINIAN_UPPER = L"АґҐБВГДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯ";
+const wstring UKRAINIAN_LOWER = L"ґґабвгдеєжзиіїйклмнопрстуфхцчшщьюя";
+
+pair<wstring, wstring> chooseAlphabet() {
+    int langChoice;
+    wcout << YELLOW << L"Choose language alphabet:\n"
+        << L"1. Ukrainian (А-Я, Ґ, Є, І, Ї)\n"
+        << L"2. Russian (А-Я)\n"
+        << L"3. Latin (A-Z)\n" << RESET;
+    wcout << BOLD << L"Option: " << RESET;
+    wcin >> langChoice;
+    wcin.ignore();
+
+    switch (langChoice) {
+    case 1: return { UKRAINIAN_UPPER, UKRAINIAN_LOWER };
+    case 2: return { RUSSIAN_UPPER, RUSSIAN_LOWER };
+    case 3: return { LATIN_UPPER, LATIN_LOWER };
+    default:
+        wcout << RED << L"Invalid choice. Defaulting to Ukrainian.\n" << RESET;
+        return { UKRAINIAN_UPPER, UKRAINIAN_LOWER };
     }
-    return encryptedText;
 }
 
-string caesarDecipher(const string& text, int startKey, int multiplier, int divisor) {
-    string decryptedText = "";
+wstring caesarCipher(const wstring& text, int startKey, int multiplier, int divisor,
+    const pair<wstring, wstring>& alphabets) {
+    wstring result = L"";
     int key = startKey;
+    int N = alphabets.first.size();
 
-    for (char charValue : text) {
-        if (isalpha(charValue)) {
-            int shift = (key * multiplier) / divisor;
-            if (islower(charValue)) {
-                charValue = ((charValue - 'a' - shift + 26) % 26) + 'a';
-            }
-            else {
-                charValue = ((charValue - 'A' - shift + 26) % 26) + 'A';
-            }
+    for (wchar_t ch : text) {
+        size_t index = alphabets.first.find(ch);
+        bool isUpper = true;
+        if (index == wstring::npos) {
+            index = alphabets.second.find(ch);
+            isUpper = false;
         }
-        decryptedText += charValue;
-        key++;
+
+        if (index != wstring::npos) {
+            int shift = (key * multiplier) / divisor;
+            int newIndex = (index + shift) % N;
+            result += isUpper ? alphabets.first[newIndex] : alphabets.second[newIndex];
+            key++;
+        }
+        else {
+            result += ch;
+        }
     }
-    return decryptedText;
+
+    return result;
 }
 
-void printLine(char ch = '=', int length = 30) {
-    for (int i = 0; i < length; i++) cout << ch;
-    cout << endl;
+wstring caesarDecipher(const wstring& text, int startKey, int multiplier, int divisor,
+    const pair<wstring, wstring>& alphabets) {
+    wstring result = L"";
+    int key = startKey;
+    int N = alphabets.first.size();
+
+    for (wchar_t ch : text) {
+        size_t index = alphabets.first.find(ch);
+        bool isUpper = true;
+        if (index == wstring::npos) {
+            index = alphabets.second.find(ch);
+            isUpper = false;
+        }
+
+        if (index != wstring::npos) {
+            int shift = (key * multiplier) / divisor;
+            int newIndex = (index + N - (shift % N)) % N;
+            result += isUpper ? alphabets.first[newIndex] : alphabets.second[newIndex];
+            key++;
+        }
+        else {
+            result += ch;
+        }
+    }
+
+    return result;
+}
+
+void printLine(wchar_t ch = L'=', int length = 30) {
+    for (int i = 0; i < length; i++) wcout << ch;
+    wcout << endl;
 }
 
 void printMenu() {
     printLine();
-    cout << BOLD << CYAN << "        CAESAR CIPHER" << RESET << endl;
+    wcout << BOLD << CYAN << L"        CAESAR CIPHER" << RESET << endl;
     printLine();
-    cout << YELLOW << "1." << RESET << " " << GREEN << "Encrypt phrase" << RESET << endl;
-    cout << YELLOW << "2." << RESET << " " << GREEN << "Decrypt phrase" << RESET << endl;
-    cout << YELLOW << "3." << RESET << " " << RED << "Exit" << RESET << endl;
+    wcout << YELLOW << L"1." << RESET << L" " << GREEN << L"Encrypt phrase" << RESET << endl;
+    wcout << YELLOW << L"2." << RESET << L" " << GREEN << L"Decrypt phrase" << RESET << endl;
+    wcout << YELLOW << L"3." << RESET << L" " << RED << L"Exit" << RESET << endl;
     printLine();
-    cout << BOLD << "Choose option: " << RESET;
+    wcout << BOLD << L"Choose option: " << RESET;
 }
 
 int main() {
+#ifdef _WIN32
+    _setmode(_fileno(stdout), _O_U16TEXT);
+    _setmode(_fileno(stdin), _O_U16TEXT);
+#endif
+
+    locale::global(locale(""));
+
     while (true) {
         printMenu();
         int choice;
-        cin >> choice;
-        cin.ignore();
+        wcin >> choice;
+        wcin.ignore();
 
-        if (choice == 1) {
-            string text;
+        if (choice == 1 || choice == 2) {
+            wstring text;
             int startKey, multiplier, divisor;
 
-            cout << MAGENTA << "Input Text: " << RESET;
-            getline(cin, text);
-            cout << MAGENTA << "Input start key: " << RESET;
-            cin >> startKey;
-            cout << MAGENTA << "Input multiplier: " << RESET;
-            cin >> multiplier;
-            cout << MAGENTA << "Input divisor: " << RESET;
-            cin >> divisor;
-            cin.ignore();
+            auto alphabets = chooseAlphabet();
 
-            string encrypted = caesarCipher(text, startKey, multiplier, divisor);
+            wcout << MAGENTA << L"Input Text: " << RESET;
+            getline(wcin, text);
+            wcout << MAGENTA << L"Input start key: " << RESET;
+            wcin >> startKey;
+            wcout << MAGENTA << L"Input multiplier: " << RESET;
+            wcin >> multiplier;
+            wcout << MAGENTA << L"Input divisor: " << RESET;
+            wcin >> divisor;
+            wcin.ignore();
 
-            cout << GREEN << "\nEncrypted text: " << BOLD << encrypted << RESET << "\n\n";
-        }
-        else if (choice == 2) {
-            string text;
-            int startKey, multiplier, divisor;
-
-            cout << MAGENTA << "Input Encrypted Text: " << RESET;
-            getline(cin, text);
-            cout << MAGENTA << "Input start key: " << RESET;
-            cin >> startKey;
-            cout << MAGENTA << "Input multiplier: " << RESET;
-            cin >> multiplier;
-            cout << MAGENTA << "Input divisor: " << RESET;
-            cin >> divisor;
-            cin.ignore();
-
-            string decrypted = caesarDecipher(text, startKey, multiplier, divisor);
-
-            cout << GREEN << "\nDecrypted text: " << BOLD << decrypted << RESET << "\n\n";
+            if (choice == 1) {
+                wstring encrypted = caesarCipher(text, startKey, multiplier, divisor, alphabets);
+                wcout << GREEN << L"\nEncrypted text: " << BOLD << encrypted << RESET << L"\n\n";
+            }
+            else {
+                wstring decrypted = caesarDecipher(text, startKey, multiplier, divisor, alphabets);
+                wcout << GREEN << L"\nDecrypted text: " << BOLD << decrypted << RESET << L"\n\n";
+            }
         }
         else if (choice == 3) {
-            cout << RED << "\nExiting program. Goodbye! :)" << RESET << endl;
+            wcout << RED << L"\nExiting program. Goodbye! :)" << RESET << endl;
             break;
         }
         else {
-            cout << RED << "Invalid choice, please try again." << RESET << endl << endl;
+            wcout << RED << L"Invalid choice, please try again." << RESET << endl << endl;
         }
     }
 
